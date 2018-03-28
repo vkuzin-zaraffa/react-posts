220053ed1a23218395be2dd9925d2d217e83cf54
Valerii <vkuzin@zaraffasoft.com>
2018-03-28 23:54:51 +0300

    #9.123 Added Routing

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import App from './components/app';
+class Hello extends React.Component {
+  render() { return <div>Hello!</div> }
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div> }
+}
+
+import PostsIndex from './components/posts_index';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
