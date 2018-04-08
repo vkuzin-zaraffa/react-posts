1264a17f02399b2891cf5bec1a426490a3999cbb
Valerii <vkuzin@zaraffasoft.com>
2018-04-08 22:43:10 +0300

    9.129 Added Swith Route and route PostsNew

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import  React, { Component } from 'react';
+
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        Posts new!
+      </div>
+    );
+  }
+}
+
+export  default  PostsNew;
