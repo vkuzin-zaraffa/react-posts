25b720027b5e8b221e9ae26ccee0a583da39eeaa
Valerii <vkuzin@zaraffasoft.com>
2018-04-08 22:58:36 +0300

    #9.131 Added link other page

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
 import { fetchPosts } from '../actions'
 
 class PostsIndex extends Component {
@@ -21,6 +23,12 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Added Post
+          </Link>
+
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
