32a0704e079a9d1f52ec6e9500a507d8eb7a8c10
Valerii <vkuzin@zaraffasoft.com>
2018-04-09 15:15:18 +0300

    #9.135 Added Form Fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,49 @@
 import  React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+          // {...field.input} =
+          // onChange={field.input.onChange}
+          // onFocus={field.input.onFocus}
+          // onBlur={field.input.onBlur}
+        />
+      </div>
+    )
+  }
+
+
   render() {
     return (
-      <div>
-        Posts new!
-      </div>
+      <form>
+        <Field
+          label="Title For Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     );
   }
 }
 
-export  default  PostsNew;
+export  default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
