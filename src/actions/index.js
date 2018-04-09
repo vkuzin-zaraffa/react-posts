67486edfa5b33204d60d0fd9fbeadfb322aa541e
Valerii <vkuzin@zaraffasoft.com>
2018-04-09 21:08:30 +0300

    #9.149 Added delete post

--- src/actions/index.js
+++ src/actions/index.js
@@ -39,3 +39,13 @@ export function fetchPost(id) {
   }
 
 }
+
+export function deletePost(id, callback) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => callback());
+
+  return {
+    type: DELETE_POST,
+    payload: id
+  }
+}
